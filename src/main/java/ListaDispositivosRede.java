dd6b8c61b9ee4dea41c3d6a18b89f1f3d8c7a7d1
Gabriel Fonseca <gabrielfonseca@edu.unifor.br>
2023-03-28 22:34:50 -0300

    Implementada multithreading e descobrimento de dispositivos em redes públicas.

--- src/main/java/ListaDispositivosRede.java
+++ src/main/java/ListaDispositivosRede.java
@@ -1,9 +1,6 @@
 import java.awt.*;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -13,34 +10,19 @@ import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import org.apache.commons.net.util.SubnetUtils;
 
 public class ListaDispositivosRede {
 
-    public static void main(String[] args) throws SocketException {
-        Graph<String, Integer> grafo = new SparseGraph<>();
+    public static void main(String[] args) {
         List<String> enderecos = new ArrayList<>();
+        Graph<String, Integer> grafo = new SparseGraph<>();
+        popularListaDeEnderecos(enderecos);
+        popularGrafoComEnderecos(enderecos, grafo);
+        renderizarJanela(grafo);
+    }
 
-        // Obter endereços IP de dispositivos conectados
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
-        while (interfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = interfaces.nextElement();
-
-            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-                continue;
-            }
-
-            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
-
-            while (addresses.hasMoreElements()) {
-                InetAddress address = addresses.nextElement();
-
-                if (address.getHostAddress().matches("\\d+\\.\\d+\\.\\d+\\.\\d+")) {
-                    enderecos.add(address.getHostAddress());
-                }
-            }
-        }
-
+    private static void popularGrafoComEnderecos(List<String> enderecos, Graph<String, Integer> grafo) {
         // Adicionar nós do grafo
         for (String endereco : enderecos) {
             grafo.addVertex(endereco);
@@ -52,10 +34,11 @@ public class ListaDispositivosRede {
                 grafo.addEdge(i * enderecos.size() + j, enderecos.get(i), enderecos.get(j));
             }
         }
+    }
 
-        // Exibir grafo visualmente
+    private static void renderizarJanela(Graph<String, Integer> grafo) {
         CircleLayout<String, Integer> layout = new CircleLayout<>(grafo);
-        layout.setSize(new Dimension(300, 300));
+        layout.setSize(new Dimension(500, 300));
         BasicVisualizationServer<String, Integer> visualizacao = new BasicVisualizationServer<>(layout);
         visualizacao.setPreferredSize(new Dimension(350, 350));
         visualizacao.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
@@ -64,5 +47,52 @@ public class ListaDispositivosRede {
         janela.getContentPane().add(visualizacao);
         janela.pack();
         janela.setVisible(true);
+    }
+
+    private static void popularListaDeEnderecos(List<String> enderecos) {
+        // Descobrir endereço IP e máscara de sub-rede da rede pública
+        SubnetUtils subnet = new SubnetUtils("192.168.1.0/24");
+        String[] ips = subnet.getInfo().getAllAddresses();
+        List<String[]> ipsRepartidos = repartirArray(ips, 255);
+        List<Thread> threads = new ArrayList<>();
+
+        // Verificar quais dispositivos estão ativos na rede pública
+        for (String[] listaIps : ipsRepartidos) {
+            Thread thread = new Thread(() -> {
+                for (String ip : listaIps) {
+                    try {
+                        if (InetAddress.getByName(ip).isReachable(1000)) {
+                            enderecos.add(ip);
+                            System.out.println(ip + " - Adicionado");
+                        }
+                    } catch (Exception ignored) {
+                    }
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    public static List<String[]> repartirArray(String[] input, int quantidade) {
+        List<String[]> retorno = new ArrayList<>();
+        int size = input.length / quantidade;
+        int remainder = input.length % quantidade;
+        int offset = 0;
+
+        for (int i = 0; i < quantidade; i++) {
+            int arraySize = size + (i < remainder ? 1 : 0);
+            String[] subArray = new String[arraySize];
+            System.arraycopy(input, offset, subArray, 0, arraySize);
+            offset += arraySize;
+            retorno.add(subArray);
+        }
+        return retorno;
     }
 }
